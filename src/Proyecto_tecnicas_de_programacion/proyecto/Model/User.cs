d2321d2b9a1f2652acd6d8986274360403aa4cd7
Gabriel Monge Monge <josuemonge9926@gmail.com>
2024-11-21 23:37:53 -0600

    commit 2 clase usuarios

---
+++ src/Proyecto_tecnicas_de_programacion/proyecto/Model/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.Model
+{/// <summary>
+///declaro las variables de los atributos de la clase user.
+/// </summary>
+    public class User
+    {
+
+        public User(int id, string nombre, string apellido, string clase){
+
+             this.clase = clase;
+             this.nombre = nombre;
+             this.apellido = apellido;
+            this.id = id;
+            }
+
+        public string nombre { get; set; }
+
+        
+
+
+      public string apellido{ get; set; }
+
+    
+
+      public string clase{ get; set; }
+
+      
+
+
+    public int id { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+}
+
