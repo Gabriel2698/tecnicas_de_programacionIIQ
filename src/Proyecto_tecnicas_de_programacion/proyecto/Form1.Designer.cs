4d32e5a7a32eff4d5119a86c7819e18ffd3a2e70
Gabriel Monge Monge <josuemonge9926@gmail.com>
2024-11-22 23:19:53 -0600

    commit 4 imagen

--- src/Proyecto_tecnicas_de_programacion/proyecto/Form1.Designer.cs
+++ src/Proyecto_tecnicas_de_programacion/proyecto/Form1.Designer.cs
@@ -28,26 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            lblNombre = new Label();
+            btnInicio = new Button();
+            lblContraseña = new Label();
+            btnCancelar = new Button();
+            txtNombre = new TextBox();
+            txtContraseña = new TextBox();
             SuspendLayout();
             // 
-            // label1
+            // lblNombre
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(208, 100);
-            label1.Name = "label1";
-            label1.Size = new Size(50, 20);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
-            label1.Click += label1_Click;
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(184, 107);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(139, 20);
+            lblNombre.TabIndex = 0;
+            lblNombre.Text = "Nombre de Usuario";
+            lblNombre.TextAlign = ContentAlignment.TopCenter;
+            lblNombre.Click += label1_Click;
+            // 
+            // btnInicio
+            // 
+            btnInicio.Location = new Point(162, 274);
+            btnInicio.Name = "btnInicio";
+            btnInicio.Size = new Size(131, 34);
+            btnInicio.TabIndex = 1;
+            btnInicio.Text = "INICIO";
+            btnInicio.UseVisualStyleBackColor = true;
+            // 
+            // lblContraseña
+            // 
+            lblContraseña.AutoSize = true;
+            lblContraseña.Location = new Point(184, 165);
+            lblContraseña.Name = "lblContraseña";
+            lblContraseña.Size = new Size(83, 20);
+            lblContraseña.TabIndex = 2;
+            lblContraseña.Text = "Contraseña";
+            lblContraseña.TextAlign = ContentAlignment.TopCenter;
+            lblContraseña.Click += label1_Click_1;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(348, 274);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(131, 34);
+            btnCancelar.TabIndex = 3;
+            btnCancelar.Text = "CANCELAR";
+            btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.Click += btnCancelar_Click;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(329, 107);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(195, 27);
+            txtNombre.TabIndex = 4;
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.Location = new Point(329, 165);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Size = new Size(195, 27);
+            txtContraseña.TabIndex = 5;
+            txtContraseña.TextChanged += textBox2_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(label1);
+            Controls.Add(txtContraseña);
+            Controls.Add(txtNombre);
+            Controls.Add(btnCancelar);
+            Controls.Add(lblContraseña);
+            Controls.Add(btnInicio);
+            Controls.Add(lblNombre);
             Name = "Form1";
+            StartPosition = FormStartPosition.Manual;
             Text = "Login";
             Load += Form1_Load;
             ResumeLayout(false);
@@ -56,6 +113,11 @@
 
         #endregion
 
-        private Label label1;
+        private Label lblNombre;
+        private Button btnInicio;
+        private Label lblContraseña;
+        private Button btnCancelar;
+        private TextBox txtNombre;
+        private TextBox txtContraseña;
     }
 }
--- src/Proyecto_tecnicas_de_programacion/proyecto/Form1.cs
+++ src/Proyecto_tecnicas_de_programacion/proyecto/Form1.cs
@@ -1,11 +1,35 @@
+using System.Diagnostics.Metrics;
+
 namespace proyecto
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
-            InitializeComponent();
+          InitializeComponent();
+            
+          
+            this.controller = new PersonController();
+            var people = controller.GetPeople();
+
+            this.listView1.Columns.Add(Text = NameColumn, Width = 100);
+            this.listView1.Columns.Add(Text = Apellido, Width = 100);
+            this.listView1.Columns.Add(Text = Country, Width = 100);
+            this.listView1.Items.AddRange(people.Select(p => new ListViewItem([p.Nombre, p.Apellido, p.Clase,p.Id])).ToArray());  
+            
+            
+            
+          
         }
+
+
+
+
+
+
+
+
+           
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -14,6 +38,24 @@ namespace proyecto
 
         private void label1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtContraseña.Clear();
+            txtNombre.Clear();
+
 
         }
     }
